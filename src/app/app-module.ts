c0a3b274143184d2514eeaba098e79625cc9baa7
Julianaoaguilar <julianaoaguilar16@gmail.com>
2026-05-21 10:27:13 -0300

    Closes #2: Create footer and header Components
    - Create Header component
    
    - Create footer component
    
    -  Header and footer components completed

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/shared/header-component/header-component';
+import { FooterComponent } from './components/shared/footer-component/footer-component';
+
 
 @NgModule({
-  declarations: [App],
+  declarations: [App, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
